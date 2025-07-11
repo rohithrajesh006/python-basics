0df9f568aefd41cff970c232cb5047ed8dd6f2b9
rohithrajesh006 <rohithrajesh006@gmail.com>
2025-07-11 08:24:21 +0530

    for loop again updated with some examples

--- for_loop_sample.py
+++ for_loop_sample.py
@@ -1,6 +1,5 @@
 #for loop-used to iterate over a sequence
-
-my_list=[11,22,33,44,55,66]
+"""my_list=[11,22,33,44,55,66]
 
 for list in my_list:
     print(list,end="|")# end=" "  to print on the same line with space
@@ -22,7 +21,7 @@ for set in my_set:
 print("")
 
 my_dict={"name":"man",11:11,"place":"earth"}
-
+"""
 """for dict in my_dict:
     print(dict,end=":")
     print(my_dict,":",[dict])#shows both key and value.
@@ -30,7 +29,7 @@ print(my_dict.items())# .tems() used in dictionary.show key value pair as tuples
 for dict in my_dict.items():
     print(dict)
 """
-for a,b in my_dict.items():
+"""for a,b in my_dict.items():
     print(a,":",b)#show both key and value.
 
 
@@ -39,4 +38,27 @@ for l in enumerate(my_newlist):#enumerate used in sequences,default 0
     print(l)
 print("")
 for m in enumerate(my_newlist,6):#starts with 6
-    print(m)
+    print(m)"""
+
+
+"""for even in range(1,11):
+    if even%2==0:
+        print(even,end=" ")"""
+
+"""print("To find the sum of numbers from a starting number to ending number")
+num1=int(input("Enter the first number :"))
+num2=int(input("Enter the second number :"))
+total=0
+for num in range(num1,num2+1):
+    total+=num
+print(f"sum of numbers from {num1} to {num2} is {total}")"""
+
+
+print("to find how many vowels present in a word")
+word=str(input("Enter the word :"))
+vowels="aeiouAEIOU"
+no_vowels=0
+for a in word:
+    if a in vowels:
+        no_vowels+=1
+print("number of vowels is :",no_vowels)

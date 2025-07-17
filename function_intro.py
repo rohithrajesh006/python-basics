c362d1311208016c814a6a6013a7c0e7d64e9112
rohithrajesh006 <rohithrajesh006@gmail.com>
2025-07-17 08:48:46 +0530

    function intro with examples

---
+++ function_intro.py
@@ -0,0 +1,70 @@
+"""def fun_name():
+    print("hii")
+
+fun_name()"""
+
+"""def fun_name1(name):
+    print("hii",name)
+
+fun_name1("athul")
+fun_name1("ashwin")"""
+
+
+"""def fact(num):
+    fact=ans=1
+    while fact<=num:
+        ans=fact*ans
+        fact+=1
+    print(ans)
+
+fact(3)
+fact(5)"""
+
+
+"""def iseven(num):
+    if num%2==0:
+        print(f"{num} is even")
+    else:
+        print(f"{num} is odd")
+a=int(input("Enter the number:"))
+iseven(a)"""
+
+
+"""def iseven(num):
+    if num%2==0:
+        return f"{num} is even"
+    else:
+        return f"{num} is odd"
+
+
+def ispositive(num):
+    if num>=0:
+        return True
+    else:
+        return False
+
+num=int(input("Enter a number:"))
+if ispositive(num):
+    print(iseven(num))
+else:
+    print("Enter a positive number")
+"""
+
+
+'''def total(num):
+    a=0
+    for b in num:
+        a=a+b
+    return a
+my_list=[1,2,3,4,5]
+d=total(my_list)
+print("average is",d/len(my_list))
+'''
+
+
+
+
+
+
+
+

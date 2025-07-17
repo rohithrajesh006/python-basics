b056dca2c5e7fa4eb5716d64fd9c1a73feb0c428
rohithrajesh006 <rohithrajesh006@gmail.com>
2025-07-17 08:46:16 +0530

    pattern updated

--- pattern.py
+++ pattern.py
@@ -40,3 +40,39 @@ print(list(range(5,0,-1)))"""
         print(a*b,end=" ")
     print(" ")"""
 
+"""for a in range(5,0,-1):
+    print(" "*int(5-a),"* "*a)"""
+
+
+"""for a in range(1,6):
+    print(" "*int(5-a),"* "*a)
+for b in range(5,0,-1):
+    print(" "*int(5-b),"* "*b)
+"""
+
+"""for a in range(1,6):
+    for b in range(a):
+        print(chr(65+b),end=" ")
+    print()"""
+
+"""for a in range(1,6):
+
+    k=a
+    n=4
+    for b in range(1,a+1):
+        print(k,end=" ")
+        k=k+n
+        n-=1
+    print()
+    """
+"""row=4
+col=5
+for a in range(row):
+    for b in range(col):
+        if a==0 or a==row-1 or  b==0 or b==col-1:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()      
+"""
+

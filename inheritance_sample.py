462ab1393bd5be19749a791bfa69b8bc54bc32c5
rohithrajesh006 <rohithrajesh006@gmail.com>
2025-08-01 13:18:42 +0530

    inheritance sample

---
+++ inheritance_sample.py
@@ -0,0 +1,17 @@
+#single inheritance
+"""class vehicles:
+    def detail(self,make,power):
+        self.make=make
+        self.power=power
+class car(vehicles):
+    def __init__(self,color,brand):
+        self.color=color
+        self.brand=brand
+c1=car("red","skoda")
+c1.detail(make=2010,power=700)
+print(c1.make)
+print(c1.brand)"""
+
+#multiple inheritance
+
+class

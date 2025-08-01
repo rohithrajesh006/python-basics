7c06619baec1d1ea722d1896a701a0176d06146c
rohithrajesh006 <rohithrajesh006@gmail.com>
2025-08-01 13:19:37 +0530

    lambda intro

---
+++ lambda_sample.py
@@ -0,0 +1,32 @@
+"""def addition(a,b):
+    return a+b
+print(addition(1,6))"""
+
+"""add=lambda a,b:a+b
+print(add(1,6))"""
+
+"""even=lambda num:True if num%2==0 else False
+print(even(3))
+"""
+"""even=lambda num:"{} is even".format(num) if num%2==0 else f"{num} is odd"
+n=int(input("Enter a number:"))
+print(even(n))"""
+
+"""def square (num):
+    return num**2
+numbers=[1,2,3]
+for a in numbers:
+    print(square(a),end=" ")"""
+
+"""def square (num):
+    return num**2
+numbers=[1,2,3]
+print(list(map(square,numbers)))"""
+
+"""numbers=[1,2,3]
+print(list(map(lambda num:num**2,numbers)))"""
+
+print(list)
+
+
+
